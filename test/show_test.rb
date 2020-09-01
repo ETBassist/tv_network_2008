22c1a953f5a6d0fa5e8bd3a52d0883466643a2ae
Eugene Theriault <hybridbassist@gmail.com>
2020-09-01 08:19:55 -0700

    Create test to see if Show exists

---
+++ test/show_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+
+class ShowTest < Minitest::Test
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_exists
+    assert_instance_of Show, @knight_rider
+  end
+
+end
