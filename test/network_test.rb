a5201f37ab156d095b0ad65c041b97bba5cca882
Eugene Theriault <hybridbassist@gmail.com>
2020-09-01 08:42:19 -0700

    Add test for attributes in Network class

--- test/network_test.rb
+++ test/network_test.rb
@@ -8,4 +8,9 @@ class NetworkTest < Minitest::Test
   def test_it_exists
     assert_instance_of Network, @nbc
   end
+
+  def test_can_get_name_and_shows
+    assert_equal "NBC", @nbc.name
+    assert_equal [], @nbc.shows
+  end
 end
